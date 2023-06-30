a0011e780623d6a4a775770a0c5320da6fc04ab1
Akshay Khot <akshay.khot@hey.com>
2023-06-30 10:17:33 -0700

    separate views

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ class App
   def call(env)
     headers = { 'Content-Type' => 'text/html' }
     
-    response = ['<h1>Hello, World!</h1>']
+    response_html = File.read 'views/index.html'
     
-    [200, headers, response]
+    [200, headers, [response_html]]
   end
 end
