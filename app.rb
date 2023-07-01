77bd40c70ba0d089227d1955999a5110260a00cd
Akshay Khot <akshay.khot@hey.com>
2023-06-30 19:50:17 -0700

    render erb views

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
+require 'erb'
+
 class App
   def call(env)
     headers = { 'Content-Type' => 'text/html' }
     
-    response_html = File.read('views/index.html')
-    
+    title = get_title(env) # new code here
+    template = ERB.new(template_html)
+    response_html = template.result(binding)
+
     [200, headers, [response_html]]
+  end
+
+  def get_title(env)
+    query = env['QUERY_STRING'] # "title=ruby"
+    values = query.split('=')   # ["title", "ruby"]
+    values[1]                   # ruby
+  end
+
+  def template_html
+    File.read 'views/index.html.erb'
   end
 end
--- config.ru
+++ config.ru
@@ -6,6 +6,6 @@ use Rack::Reloader, 0
 
 # Serve all requests beginning with /public 
 # from the "public" folder 
-use Rack::Static, urls: ['/public']
+use Rack::Static, urls: ['/public', "/favicon.ico"]
 
 run App.new
