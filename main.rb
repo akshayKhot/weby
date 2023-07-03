afff5d9b285d77abf1e8757dcbc92ae8cdc884fc
Akshay Khot <akshay.khot@hey.com>
2023-07-03 12:13:22 -0700

    update main

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
-require 'erb'
+Router.draw do
+  get('/') { 'Hello World' }
 
-name = 'Akshay'
-age = 30
+  get('/posts') { 'All Posts' }
 
-template = ERB.new 'My name is <%= name %> and I am <%= age %> years old'
-puts template.result(binding)
+  get('/posts/new') { 'New post' }
+
+  # Access HTTP request, too!
+  get('/foo') do |env|
+    "You're visiting #{env['REQUEST_URI']}"
+  end
+end
