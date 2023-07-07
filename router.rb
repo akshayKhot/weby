3f2dd4e71bb83a052d9db9975db4001bc9a73258
Akshay Khot <akshay.khot@hey.com>
2023-07-06 19:05:38 -0700

    add comments

--- router.rb
+++ router.rb
@@ -20,12 +20,12 @@ class Router
     if blk
       @routes[path] = blk
     else
-      if path.include? '#'
-        controller, action = path.split('#')
-        controller_klass_name = controller.capitalize + 'Controller'
-        controller_klass = Object.const_get(controller_klass_name)
-        handler = controller_klass.new.method(action.to_sym)
-        @routes[path.gsub('#', '/').prepend('/')] = handler.to_proc
+      if path.include? '#'  # 'application#index'
+        controller, action = path.split('#')  # 'application', 'index'
+        controller_klass_name = controller.capitalize + 'Controller'  # 'ApplicationController'
+        controller_klass = Object.const_get(controller_klass_name)  # ApplicationController
+        handler = controller_klass.new.method(action.to_sym)  # index
+        @routes[path.gsub('#', '/').prepend('/')] = handler.to_proc  #{ '/application/index' => Proc(index) }
       end
     end
   end
