4e5493916bb3290b95dc76a9f7389b5660e7cf4e
Akshay Khot <akshay.khot@hey.com>
2023-07-06 21:01:18 -0700

    move to controller-folder

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,6 +1,9 @@
+require 'debug'
+
 class ApplicationController
   def render(view, data)
-    view_file = "views/#{view.to_s}.html.erb"
+    controller_name = self.class.name.downcase.split('controller').first
+    view_file = "views/#{controller_name}/#{view.to_s}.html.erb"
     template = ERB.new File.read(view_file)
     template.result(binding)
   end
