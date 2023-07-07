ad0912528a0b7a3160fe545a398fba4731d7ca0f
Akshay Khot <akshay.khot@hey.com>
2023-07-06 20:29:41 -0700

    change route format

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Router.draw do
   get('/') { "Akshay's Blog" }
   
   get('/articles') { 'All Articles' }
-  
-  get('/articles/1') do |env| 
-    puts "Path: #{env['REQUEST_PATH']}"
-    "First Article"
-  end
 
-  get 'home#index'
+  get 'home/index'
 end
--- router.rb
+++ router.rb
@@ -20,12 +20,12 @@ class Router
     if blk
       @routes[path] = blk
     else
-      if path.include? '#'  # 'home#index'
-        controller, action = path.split('#')  # 'home', 'index'
+      if path.include? '/'  # 'home/index'
+        controller, action = path.split('/')  # 'home', 'index'
         controller_klass_name = controller.capitalize + 'Controller'  # 'HomeController'
         controller_klass = Object.const_get(controller_klass_name)  # HomeController
         handler = controller_klass.new.method(action.to_sym)  # index
-        @routes[path.gsub('#', '/').prepend('/')] = handler.to_proc  # { '/home/index' => Proc(index) }
+        @routes[path.prepend('/')] = handler.to_proc  # { '/home/index' => Proc(index) }
       end
     end
   end
