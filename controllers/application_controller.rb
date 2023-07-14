0fc1992177b415184f26a64154ac07d713dd2a86
Akshay Khot <akshay.khot@hey.com>
2023-07-14 14:31:38 -0700

    move to base controller

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController
   def initialize(env)
     @env = env
   end
+
+  def render(view_template)
+    erb_template = ERB.new File.read(view_template)
+    erb_template.result(get_binding)
+  end
+
+  def get_binding
+    binding
+  end
 end
--- controllers/articles_controller.rb
+++ controllers/articles_controller.rb
@@ -6,13 +6,4 @@ class ArticlesController < ApplicationController
     @title = 'All Articles'
     @name = 'Akshay'
   end
-
-  def render(view_template)
-    erb_template = ERB.new File.read(view_template)
-    erb_template.result(get_binding)
-  end
-
-  def get_binding
-    binding
-  end
 end
