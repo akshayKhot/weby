ce14201dc8c603f603363bad67be73ec29ce0e7b
Akshay Khot <akshay.khot@hey.com>
2023-07-14 14:42:58 -0700

    refactor

--- router.rb
+++ router.rb
@@ -21,14 +21,13 @@ class Router
     if blk
       @routes[path] = blk
     else
-      if path.include? '/'                              # 'articles/index'
+      if path.include? '/'                                # 'articles/index'
         @routes['/' + path] = ->(env) {
           controller_name, action_name = path.split('/')  # 'articles', 'index'
-          controller_klass_name = controller_name.capitalize + 'Controller'   # 'ArticlesController'
-          controller_klass = Object.const_get(controller_klass_name)          # ArticlesController
+          controller_klass = constantize(controller_name) # ArticlesController
+          kontroller = controller_klass.new(env)          # controller = ArticlesController.new(env)
 
-          kontroller = controller_klass.new(env)  # controller = ArticlesController.new(env)
-          kontroller.send(action_name.to_sym)     # controller.index
+          kontroller.send(action_name.to_sym)             # controller.index
           kontroller.render("views/#{controller_name}/#{action_name}.html.erb")
         }
       end
@@ -39,5 +38,12 @@ class Router
     path = env['REQUEST_PATH']
     handler = @routes[path] || ->(env) { "no route found for #{path}" }
     handler.call(env)
+  end
+
+  # input: 'articles'
+  # output: ArticlesController
+  def constantize(name)
+    controller_klass_name = name.capitalize + 'Controller'  # 'ArticlesController'
+    Object.const_get(controller_klass_name)
   end
 end
