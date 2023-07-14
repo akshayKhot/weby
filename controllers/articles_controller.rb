708a09f444be939a1388cc8d9f139b4831306992
Akshay Khot <akshay.khot@hey.com>
2023-07-14 13:51:40 -0700

    simple implementation

--- controllers/articles_controller.rb
+++ controllers/articles_controller.rb
@@ -4,22 +4,18 @@ require_relative 'application_controller'
 class ArticlesController < ApplicationController
   def index
     @title = 'All Articles'
+    @name = 'Akshay'
     render :index
   end
 
   def render(view)
-    bind = binding
-    view_variables.each do |var|
-      bind.local_variable_set(var[1..-1].to_sym, self.instance_variable_get(var))
-    end
-
     controller_name = self.class.name.downcase.split('controller').first  # articles
     view_file = "views/#{controller_name}/#{view.to_s}.html.erb"  # views/articles/index.html.erb
     template = ERB.new File.read(view_file)
-    template.result(bind)
+    template.result(get_binding)
   end
 
-  def view_variables
-    self.instance_variables - [:@env]
+  def get_binding
+    binding
   end
 end
